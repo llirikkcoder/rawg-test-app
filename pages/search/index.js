2852a181fa7012841830382cf15f6a6f7559a725
Kirill Pavliashik <kpavliashik@netevia.com>
2023-04-14 22:05:30 +0400

    in progress

--- pages/search/index.js
+++ pages/search/index.js
@@ -69,6 +69,74 @@ const ResultsGrid = styled.div`
 }
 `
 
+const FilterContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    @media (min-width: 768px) {
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+
+    label {
+        font-weight: bold;
+    }
+
+    select,
+    input[type='checkbox'] {
+        padding: 0.5rem;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        font-size: 1rem;
+    }
+
+    select {
+        width: 100%;
+        max-width: 200px;
+    }
+`
+
+const CheckboxGroup = ({ id, options, values = [], onChange }) => {
+    const handleChange = (event) => {
+        const targetValue = event.target.value
+        const isChecked = event.target.checked
+
+        let newValues = [...values]
+
+        if (isChecked) {
+            newValues.push(targetValue)
+        } else {
+            newValues = newValues.filter((value) => value !== targetValue)
+        }
+
+        onChange(newValues)
+    }
+
+    return (
+        <div>
+            {options.map(({ label, value }) => (
+                <label key={value}>
+                    <input
+                        type='checkbox'
+                        id={`${id}-${value}`}
+                        value={value}
+                        checked={values?.includes(value)}
+                        onChange={handleChange}
+                    />
+                    {label}
+                </label>
+            ))}
+        </div>
+    )
+}
+
 const SearchResults = () => {
     const router = useRouter()
     const searchQuery = router?.query.s
@@ -92,6 +160,33 @@ const SearchResults = () => {
         )
     const nextAvailable = data?.pages?.map((data) => data.data.next)[0]
 
+    const handleSortChange = (event) => {
+        const newSort = event.target.value
+        fetchNextPage({
+            pageParam: 1,
+            sort: newSort,
+            platforms: router.query.platforms,
+        })
+    }
+
+    const handlePlatformChange = (event) => {
+        const platform = event.target?.value
+        const checked = event.target?.checked
+
+        let platforms = []
+        if (checked) {
+            platforms = [...router.query.platforms, platform]
+        } else {
+            platforms = router.query.platforms?.filter((p) => p !== platform)
+        }
+
+        fetchNextPage({
+            pageParam: 1,
+            sort: router.query.sort,
+            platforms: platforms,
+        })
+    }
+
     if (status === 'loading')
         return (
             <LoadingContainer>
@@ -108,6 +203,30 @@ const SearchResults = () => {
         <StyledSearchResults>
             <h1>Search Results:</h1>
 
+            <FilterContainer>
+                <div>
+                    <label htmlFor='sort'>Sort by:</label>
+                    <select id='sort' onChange={handleSortChange}>
+                        <option value='rating'>Rating</option>
+                        <option value='released'>Release Date</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor='platforms'>Platforms:</label>
+                    <CheckboxGroup
+                        id='platforms'
+                        options={[
+                            { label: 'PC', value: '4' },
+                            { label: 'PlayStation', value: '18' },
+                            { label: 'Xbox', value: '1,186' },
+                            { label: 'Nintendo', value: '7,8,9,13,21' },
+                        ]}
+                        values={router.query.platforms}
+                        onChange={handlePlatformChange}
+                    />
+                </div>
+            </FilterContainer>
+
             <ResultsGrid ref={resultsRef}>
                 {data?.pages?.map((data) =>
                     data?.data?.results?.map((game) => (
@@ -115,7 +234,6 @@ const SearchResults = () => {
                     ))
                 )}
             </ResultsGrid>
-
             <LoadMore>
                 {isFetchingNextPage ? (
                     <Oval
@@ -124,10 +242,15 @@ const SearchResults = () => {
                         color='grey'
                         ariaLabel='loading'
                     />
-                ) : (
-                    <button onClick={fetchNextPage} disabled={!nextAvailable}>
-                        {nextAvailable === null ? 'Nothing' : 'Load More'}
+                ) : nextAvailable ? (
+                    <button
+                        onClick={() => fetchNextPage()}
+                        disabled={isFetchingNextPage}
+                    >
+                        Load more
                     </button>
+                ) : (
+                    <p>No more results.</p>
                 )}
             </LoadMore>
         </StyledSearchResults>
